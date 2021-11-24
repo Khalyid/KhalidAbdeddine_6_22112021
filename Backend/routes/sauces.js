8e21cd46094df4cc843f65d33faa2dc50a1f6600
Khalid Abd <k.abdeddine@gmail.com>
2021-11-24 12:16:07 +0100

    Création systeme d'authentification sécurisé

--- Backend/routes/sauces.js
+++ Backend/routes/sauces.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
 
-router.post('/', saucesCtrl.createSauces );
-router.get('/', saucesCtrl.modifySauces);
+router.post('/', auth, saucesCtrl.createSauces );
+router.get('/', auth, saucesCtrl.modifySauces);
 
   module.exports = router;
